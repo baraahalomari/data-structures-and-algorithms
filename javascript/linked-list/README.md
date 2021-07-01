4b1557c50c3bfbeb6a0efeecb9fa686b6173d3b9
baraahalomari <balomari995@gmail.com>
2021-07-01 17:49:34 +0300

    linked-list

---
+++ javascript/linked-list/README.md
@@ -0,0 +1,23 @@
+# Singly Linked List
+
+> Linked List is a sequence of Nodes that are connected to each other.
+
+>If we want to add a node with an O(1) efficiency, we have to replace the current Head of the linked list with the new node, without losing the reference to the next node in the list.
+
+> Adding a node to the middle of a linked list is a bit different than adding to the beginning. This is because we are working with more nodes and must re-allocate to make room for the new node.
+
+
+## Challenge
+
+Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
+
+
+## Approach & Efficiency
+write the problem domaine and the algoritheme of the code then write the code stipe by stipe and test it.
+
+## API
+insert : Adds a new node with that value to the head of the list with an O(1) Time performance.
+includes : Indicates whether that value exists as a Node’s value somewhere within the list.
+to string : Returns: a string representing all the values in the Linked List.
+
+
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,65 @@
 'use strict';
-
-class LinkedList {
-
+class Node {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
 }
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    insert(value) {
 
+      try{
+        let addNode = new Node(value);
+        if (!this.head) {
+            this.head = addNode;
+        } else {
+            addNode.next = this.head;
+            this.head = addNode;
+        }
+      }catch(err){
+        console.error(err)
+      }
+        
+    }
+    includes(value) {
+      try{
+         let current = this.head;
+        if (!this.head) {
+            return false;
+        }
+        while (current.next) {
+            if (current.value == value) {
+                return true;
+            }
+            current = current.next;
+            if ((current.next == null) && (value == current.value)) {
+                return true;
+            }
+        }
+        return false;
+      }catch(err){
+        console.error(err)
+      }
+       
+    }
+    toString() {
+        let outPut = '';
+        let current = this.head;
+        while (current.next) {
+            if(current.value==null||current.value==undefined){
+                outPut = outPut + `NULL -> `;
+            }else{
+                outPut = outPut + `{${current.value}} -> `;
+            }
+            current = current.next;
+            if (current.next == null) {
+                outPut = outPut + `{${current.value}}`;
+            }
+        }
+        return outPut;
+    }
+}
 module.exports = LinkedList;
